96a2b4ec400985d8ce96e0a28791d36f62dbf6ad
Marcelo <m.henriquez11@ufromail.cl>
2022-06-27 17:08:23 -0400

    Segundo commit

--- src/main/java/Prueba.java
+++ src/main/java/Prueba.java
@@ -4,5 +4,8 @@ public class Prueba {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
         int a, b;
+        System.out.println("Ingrese dos números: ");
+        a = leer.nextInt();
+        b = leer.nextInt();
     }
 }
